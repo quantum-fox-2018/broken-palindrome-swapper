98f61c5401ded25fcc28f244d96a0f61833f3fff
annisaayu <annisaayu569@gmail.com>
2018-03-06 10:58:22 +0700

    update palindromeSwapper.js

--- palindromeSwapper.js
+++ palindromeSwapper.js
@@ -5,22 +5,23 @@
  * @param {string} str - input kata yang ingin di-swap dan dicek palindrom
  * @returns {boolean} true bila kata yang di-swap adalah palindrom
  */
-function palindromeSwapper(str) {
-  let newStr = "";
-  for (let i = 0; i < str.length; i++) {
-    for (let j = 0; j < str.length; i++) {
-      if (i === j) {
-        newStr += str[j++];
-        newStr += str[j];
-        j++;
-      } else {
-        newStr += str[j];
-      }
-    }
-    if (isPalindrome(newStr)) return true;
-  }
-  return false;
-}
+ function palindromeSwapper(str) {
+   let newStr = "";
+   for (let i = 0; i < str.length; i++) {
+     for (let j = 0; j < str.length; j++) {
+       if (i === j) {
+         newStr += str[j+1];
+         newStr += str[j];
+         j++;
+       } else {
+         newStr += str[j];
+       }
+
+       if (isPalindrome(newStr)){return true} ;
+     }
+   }
+   return false;
+ }
 
 /**
  * @function isPalindrome
@@ -28,19 +29,20 @@ function palindromeSwapper(str) {
  * @param {string} str - input kata yang dicek bila palindrom
  * @returns {boolean} true bila kata adalah palindrom
  */
-function isPalindrome(str) {
-  if (
-    str
-      .split("")
-      .reverse()
-      .join() === str
-  )
-    return console.log(true);
-  return console.log(false);
-}
+ function isPalindrome(str) {
+   if (
+     str
+       .split("")
+       .reverse()
+       .join("") === str
+   ){
+       return true;
+   }
+   return false;
+ }
 
-console.log(palindromeSwapper("arcecar")); // TRUE
-console.log(palindromeSwapper("racecar")); // TRUE
-console.log(palindromeSwapper("recacar")); // FALSE
+ console.log(palindromeSwapper("arcecar")); // TRUE
+ console.log(palindromeSwapper("racecar")); // TRUE
+ console.log(palindromeSwapper("recacar")); // FALSE
 
 module.exports = palindromeSwapper;
